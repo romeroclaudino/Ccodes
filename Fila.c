fbcf48514351d17af947563ad72e6fec4c98ecfd
Romero Claudino <romeroclaudino@users.noreply.github.com>
2015-05-08 01:32:44 -0300

    firstOnes

---
+++ Fila.c
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+struct Node
+{
+    int num;
+    struct Node* prox;
+};
+typedef struct Node node;
+
+//Protótipos
+int obtemResposta(void);
+void escolhe(int resposta, node* Fila);
+int vazia(node* Fila);
+void criaFila(node* Fila);
+void limpaFila(node* Fila);
+void exibeFila(node* Fila);
+void novoComeco(node* Fila);
+void novoFinal(node* Fila);
+void removePrimeiro(node* Fila);
+void removeUltimo(node* Fila);
+int contador(node* Fila);
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+    node* Fila = (node*) malloc(sizeof(node));
+
+    if(!Fila)
+    {
+        puts("\nNão há memória suficiente!");
+        exit(1);
+    }
+    else
+    {
+        criaFila(Fila);
+        int resposta;
+        do
+        {
+            resposta = obtemResposta();
+            escolhe(resposta, Fila);
+        }while(resposta);
+    }
+    free(Fila);
+    return 0;
+}
+
+int obtemResposta()
+{
+    puts("\n\t\t\tEscolha uma das opções abaixo:\n");
+    puts("0 - Sair.");
+    puts("1 - Exibir elementos.");
+    puts("2 - Inserir elementos no início da fila.");
+    puts("3 - Inserir elementos no final da fila.");
+    puts("4 - Remover primeiro elemento da fila.");
+    puts("5 - Remover último elemento da fila.");
+    puts("5 - Limpar a fila.");
+
+    int resposta;  printf("\nOpção: ");  scanf("%d", &resposta);
+    return resposta;
+}
+
+void escolhe(int resposta, node* Fila)
+{
+    switch(resposta)
+    {
+        case 0:
+            limpaFila(Fila);
+            break;
+        case 1:
+            exibeFila(Fila);
+            break;
+        case 2:
+            novoComeco(Fila);
+            break;
+        case 3:
+            novoFinal(Fila);
+            break;
+        case 4:
+            removePrimeiro(Fila);
+            break;
+        case 5:
+            removeUltimo(Fila);
+            break;
+        case 6:
+            criaFila(Fila);
+            break;
+        default:
+            puts("\nOpção inválida! Verifique novamente as opções apresentadas.");
+    }
+}
+
+int vazia(node* Fila)
+{
+    if(Fila->prox == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+void criaFila(node* Fila)
+{
+    Fila->prox = NULL;
+}
+
+void limpaFila(node* Fila)
+{
+    if(!vazia(Fila))
+    {
+        node* tmp = Fila->prox;
+        while(tmp != NULL)
+        {
+            tmp = Fila->prox;
+            Fila->prox = tmp->prox;
+            free(tmp);
+        }
+    }
+}
+
+void exibeFila(node* Fila)
+{
+    if(!vazia(Fila))
+    {
+        node* tmp = Fila->prox;  printf("\nElementos da fila: ");
+        while(tmp != NULL)
+        {
+            printf("%d ", tmp->num);
+            tmp = tmp->prox;
+        }
+        puts("");
+    }
+    else
+    {
+        puts("\nA fila está vazia!");
+    }
+}
+
+void novoComeco(node* Fila)
+{
+    node* novoItem = (node*) malloc(sizeof(node));
+    if(!novoItem)
+    {
+        puts("\nNão há memória suficiente!");
+        exit(1);
+    }
+    else
+    {
+        printf("\nInsira o número: ");  scanf("%d", &novoItem->num);
+        node* velhoComeco = Fila->prox;
+        Fila->prox = novoItem;
+        novoItem->prox = velhoComeco;
+    }
+}
+
+void novoFinal(node* Fila)
+{
+    node* novoItem = (node*) malloc(sizeof(node));
+    if(!novoItem)
+    {
+        puts("\nNão há memória suficiente!");
+        exit(1);
+    }
+    else
+    {
+        printf("\nInsira o número: ");  scanf("%d", &novoItem->num);
+        if(vazia(Fila))
+            Fila->prox = novoItem;
+        else
+        {
+            node* tmp = Fila->prox;
+            while(tmp->prox != NULL)
+                tmp = tmp->prox;
+            tmp->prox = novoItem;
+        }
+    }
+}
+
+void removePrimeiro(node* Fila)
+{
+    if(vazia(Fila))
+        puts("\nA fila está vazia!");
+    else
+    {
+        node* tmp = Fila->prox;
+        Fila->prox = tmp->prox;
+        free(tmp);
+    }
+}
+
+void removeUltimo(node* Fila)
+{
+    if(vazia(Fila))
+        puts("\nA fila está vazia!");
+    else
+    {
+        if(contador(Fila) == 0)
+            criaFila(Fila);
+        else
+        {
+            node* tmp = Fila->prox;
+            node* tmp2 = tmp->prox;
+            while(tmp2->prox != NULL)
+            {
+                tmp = tmp->prox;
+                tmp2 = tmp->prox;
+            }
+            tmp->prox = NULL;
+            free(tmp2);
+        }
+    }
+}
+
+int contador(node* Fila)
+{
+    int index = 0;
+    node* tmp = Fila->prox;
+    while(tmp->prox != NULL)
+    {
+        tmp = tmp->prox;
+        index++;
+    }
+    return index;
+}
---
+++ Pilha.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+struct Node
+{
+    int num;
+    struct Node* prox;
+};
+typedef struct Node node;
+
+////Protótipos
+int obtemResposta(void);
+void escolhe(int resposta, node* Pilha);
+int vazia(node* Pilha);
+void criaPilha(node* Pilha);
+void limpaPilha(node* Pilha);
+void exibePilha(node* Pilha);
+void insereItem(node* Pilha);
+void removeItem(node* Pilha);
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+    node* Pilha = (node*) malloc(sizeof(node));
+
+    if(!Pilha)
+    {
+        puts("Não há memória suficiente!");
+        exit(1);
+    }
+    else
+    {
+        criaPilha(Pilha);
+        int resposta;
+        do
+        {
+            resposta = obtemResposta();
+            escolhe(resposta, Pilha);
+        }while(resposta);
+    }
+    free(Pilha);
+    return 0;
+}
+
+int obtemResposta()
+{
+    puts("\n\t\t\tEscolha uma das opções abaixo:\n");
+    puts("0 - Sair.");
+    puts("1 - Exibir elementos.");
+    puts("2 - Inserir elementos.");
+    puts("3 - Remover elementos.");
+    puts("4 - Limpar Pilha");
+
+    int resposta; printf("\nOpção: "); scanf("%d", &resposta);
+    return resposta;
+}
+
+void escolhe(int resposta, node* Pilha)
+{
+    switch(resposta)
+    {
+        case 0:
+            limpaPilha(Pilha);
+            break;
+        case 1:
+            exibePilha(Pilha);
+            break;
+        case 2:
+            insereItem(Pilha);
+            break;
+        case 3:
+            removeItem(Pilha);
+            break;
+        case 4:
+            criaPilha(Pilha);
+            break;
+        default:
+            puts("\nOpção inválida! Verifique novamente as opções apresentadas.");
+    }
+
+}
+
+int vazia(node* Pilha)
+{
+    if(Pilha->prox == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+void criaPilha(node* Pilha)
+{
+    Pilha->prox = NULL;
+}
+
+void limpaPilha(node* Pilha)
+{
+    if(!vazia(Pilha))
+    {
+        node* tmp = Pilha->prox;
+        while(tmp != NULL)
+        {
+            tmp = Pilha->prox;
+            Pilha->prox = tmp->prox;
+            free(tmp);
+        }
+    }
+}
+
+void exibePilha(node* Pilha)
+{
+    if(!vazia(Pilha))
+    {
+        node* tmp = Pilha->prox;  printf("\nItens da lista: ");
+        while(tmp != NULL)
+        {
+            printf("%d ", tmp->num);
+            tmp = tmp->prox;
+        }
+        puts("");
+    }
+    else
+        puts("\nA Pilha está vazia!");
+}
+
+void insereItem(node* Pilha)
+{
+    node* novoItem = (node*) malloc(sizeof(node));
+    if(!novoItem)
+    {
+        puts("\nNão há memória suficiente!");
+        exit(1);
+    }
+    else
+    {
+        printf("\nInsira o número: ");  scanf("%d", &novoItem->num);
+        node* velhoComeco = Pilha->prox;
+        Pilha->prox = novoItem;
+        novoItem->prox = velhoComeco;
+    }
+}
+
+void removeItem(node* Pilha)
+{
+    if(!vazia(Pilha))
+    {
+        node* tmp = Pilha->prox;
+        Pilha->prox = tmp->prox;
+        free(tmp);
+    }
+    else
+        puts("\nA Pilha está vazia!");
+}
